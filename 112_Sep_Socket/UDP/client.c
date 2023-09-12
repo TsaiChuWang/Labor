37991d17becb8c7326338538125aec70657d083b
TsaiChuWang <wtsaichu@gmail.com>
2023-09-13 00:05:59 +0800

    UDP Socket client code aktualisieren

--- 112_Sep_Socket/UDP/client.c
+++ 112_Sep_Socket/UDP/client.c
@@ -4,26 +4,49 @@
 #include <netinet/in.h>
 #include <string.h>
 
- #define SERVER_PORT 1996
- #define BUFF_LEN 512
- #define SERVER_IP "172.0.5.182"
+#ifndef _DEBUG_COLOR_
+#define _DEBUG_COLOR_
+    #define KDRK "\x1B[0;30m"
+    #define KGRY "\x1B[1;30m"
+    #define KRED "\x1B[0;31m"
+    #define KRED_L "\x1B[1;31m"
+    #define KGRN "\x1B[0;32m"
+    #define KGRN_L "\x1B[1;32m"
+    #define KYEL "\x1B[0;33m"
+    #define KYEL_L "\x1B[1;33m"
+    #define KBLU "\x1B[0;34m"
+    #define KBLU_L "\x1B[1;34m"
+    #define KMAG "\x1B[0;35m"
+    #define KMAG_L "\x1B[1;35m"
+    #define KCYN "\x1B[0;36m"
+    #define KCYN_L "\x1B[1;36m"
+    #define WHITE "\x1B[0;37m"
+    #define WHITE_L "\x1B[1;37m"
+    #define RESET "\x1B[0m"
+#endif
 
+#define SERVER_PORT 1996
+#define BUFF_LEN 512
+#define SERVER_IP "127.0.0.1"
 
- void udp_msg_sender( int fd, struct sockaddr* dst)
+ void udp_msg_sender(int fd, struct sockaddr* dst_address,char *message)
  {
+    socklen_t length;
+    struct sockaddr_in src_address; // 來源 IP address
+    while(1)
+    {
+        char buffer[BUFF_LEN];
+        strncpy(buffer, message, sizeof(BUFF_LEN)); // 將指針中的字符複製到數組
+        buffer[strlen(message)] = '\0'; // 字串結尾
 
-     socklen_t len;
-     struct sockaddr_in src;
-     while ( 1 )
-     {
-         char buf[BUFF_LEN] = " TEST UDP MSG!\n " ;
-         len = sizeof (* dst);
-         printf( " client:%s\n " ,buf);   //打印自己發送的信息
-         sendto(fd, buf, BUFF_LEN, 0 , dst, len);
-         memset(buf, 0 , BUFF_LEN);
-         recvfrom(fd, buf, BUFF_LEN, 0 , ( struct sockaddr*)&src, &len);   //接收來自server的信息
-         printf( " server:%s\n " ,buf);
-         sleep( 1 );   //一秒發送一次消息
+        length = sizeof(*dst_address);
+        printf(KYEL"client : %s \n",message);   //打印自己發送的信息
+        sendto(fd, buffer, BUFF_LEN, 0, dst_address, length);
+        
+        memset(buffer, 0, BUFF_LEN);    // 初始化 buffer 為 0
+        recvfrom(fd, buffer, BUFF_LEN, 0, (struct sockaddr*)&src_address, &length);   //接收來自server的信息
+        printf(KCYN"server : %25s\n" ,buffer);
+        sleep(1);   //一秒發送一次消息
      }
  }
 
@@ -34,25 +57,25 @@
 
  int main( int argc, char * argv[])
  {
-     int client_fd;
-     struct sockaddr_in ser_addr;
+    int client_dentifier;
+    struct sockaddr_in server_addr;
 
-     client_fd = socket(AF_INET, SOCK_DGRAM, 0 );
-     if (client_fd < 0 )
-     {
-         printf( " create socket fail!\n " );
-         return - 1 ;
+    client_dentifier = socket(AF_INET, SOCK_DGRAM, 0);
+    if(client_dentifier < 0)
+    {
+        perror("socket");   // 打印帶有錯誤描述的錯誤消息
+        exit(-1);   // 以 -1 結束程式;
      }
 
-     memset(&ser_addr , 0 , sizeof (ser_addr));
-     ser_addr.sin_family = AF_INET;
-     // ser_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);   //注意網絡序轉換
-     ser_addr.sin_port = htons(SERVER_PORT);   //注意網絡序轉換
+    memset(&server_addr , 0, sizeof(server_addr));  // 初始化 server_addr 為 0
+    
+    server_addr.sin_family = AF_INET;
+    // server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);   //注意網絡序轉換
+    server_addr.sin_port = htons(SERVER_PORT);   //注意網絡序轉換
 
-     udp_msg_sender(client_fd, ( struct sockaddr*)& ser_addr);
+    udp_msg_sender(client_dentifier, (struct sockaddr*)& server_addr, argv[1]);
 
-     close(client_fd);
-
-     return  0 ;
+    close(client_dentifier);    // 關閉 udp socket
+    return  0 ;
 }
