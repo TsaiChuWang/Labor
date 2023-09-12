3372284886276913cb951cdf44dfaab82bdca2dd
TsaiChuWang <wtsaichu@gmail.com>
2023-09-12 22:28:54 +0800

    UDP Socket code aktualisieren

--- 112_Sep_Socket/UDP/client.c
+++ 112_Sep_Socket/UDP/client.c
@@ -1,47 +1,58 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 
-#define SERVER_IP "127.0.0.1" // The IP address of the server
-#define PORT 12345            // The port number of the server
+ #define SERVER_PORT 8888
+ #define BUFF_LEN 512
+ #define SERVER_IP "172.0.5.182"
 
-int main() {
-    int sockfd;
-    struct sockaddr_in server_addr;
-    char message[] = "Hello, server!";
-    char buffer[1024];
 
-    // Create a UDP socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("Socket creation failed");
-        exit(1);
-    }
+ void udp_msg_sender( int fd, struct sockaddr* dst)
+ {
 
-    // Initialize server address
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server_addr.sin_port = htons(PORT);
+     socklen_t len;
+     struct sockaddr_in src;
+     while ( 1 )
+     {
+         char buf[BUFF_LEN] = " TEST UDP MSG!\n " ;
+         len = sizeof (* dst);
+         printf( " client:%s\n " ,buf);   //打印自己發送的信息
+         sendto(fd, buf, BUFF_LEN, 0 , dst, len);
+         memset(buf, 0 , BUFF_LEN);
+         recvfrom(fd, buf, BUFF_LEN, 0 , ( struct sockaddr*)&src, &len);   //接收來自server的信息
+         printf( " server:%s\n " ,buf);
+         sleep( 1 );   //一秒發送一次消息
+     }
+ }
 
-    // Send a message to the server
-    sendto(sockfd, message, strlen(message), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+ /*
+     client:
+             socket-->sendto-->revcfrom-->close
+ */
 
-    // Receive a response from the server
-    int bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL);
-    if (bytes_received < 0) {
-        perror("Error receiving data");
-        exit(1);
-    }
+ int main( int argc, char * argv[])
+ {
+     int client_fd;
+     struct sockaddr_in ser_addr;
 
-    // Null-terminate the received data
-    buffer[bytes_received] = '\0';
+     client_fd = socket(AF_INET, SOCK_DGRAM, 0 );
+     if (client_fd < 0 )
+     {
+         printf( " create socket fail!\n " );
+         return - 1 ;
+     }
 
-    printf("Received from server: %s\n", buffer);
+     memset(&ser_addr , 0 , sizeof (ser_addr));
+     ser_addr.sin_family = AF_INET;
+     // ser_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);   //注意網絡序轉換
+     ser_addr.sin_port = htons(SERVER_PORT);   //注意網絡序轉換
 
-    close(sockfd);
+     udp_msg_sender(client_fd, ( struct sockaddr*)& ser_addr);
 
-    return 0;
+     close(client_fd);
+
+     return  0 ;
 }
