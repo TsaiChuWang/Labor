1befa5ee6c153c422178ed2f610bccc68eccda25
TsaiChuWang <wtsaichu@gmail.com>
2024-03-22 21:18:34 +0800

    ausführbar constraint 1 aktausilieren

--- Obkivious_routing/Programm/Enthalten/Prüfung.h
+++ Obkivious_routing/Programm/Enthalten/Prüfung.h
@@ -1,6 +1,7 @@
 #define DATEI_NAME "ausführbar"
 
 #define ANZAHL_KANTEN 12    // Number of edges
+#define ANZAHL_KNOTEN 6     // Number of nodes
 
 #ifdef ALLE_GLEICHE_KAPAZITÄT// Each link has same capacity 
     #define KAPAZITÄT 77
--- Obkivious_routing/Programm/Quelle/ausführbar.c
+++ Obkivious_routing/Programm/Quelle/ausführbar.c
@@ -25,14 +25,24 @@ int main(void){
 
     for(int kant_eins=1; kant_eins<=ANZAHL_KANTEN; kant_eins++){
         for(int kant_zwei=1; kant_zwei<=ANZAHL_KANTEN; kant_zwei++){
-            fprintf(dateizeiger, "%d pi_e%02d(h%02d)", (int)KAPAZITÄT, kant_eins, kant_zwei);
+            fprintf(dateizeiger, "%d pi_e%02d(h%02d)", KAPAZITÄT, kant_eins, kant_zwei);
             if(kant_zwei == ANZAHL_KANTEN)
                 fprintf(dateizeiger, " <= r\n");
             else fprintf(dateizeiger, " + ");
         }
     }
 
-    // for(int umrande)
+    fprintf(dateizeiger, "\n\n");
+
+    // Constraints 1: \forall i,j\in N,i\neq j p_e(i,j)+s^+_e(i,j)-s^-_e(i,j)=f_{ij}(e)/cap(e)
+    for(int kant=1; kant<=ANZAHL_KANTEN; kant++){
+        for(int knote_i = 1; knote_i<ANZAHL_KNOTEN; knote_i++)
+            for(int knote_j = 1; knote_j<ANZAHL_KNOTEN; knote_j++)
+                if(knote_i != knote_j)
+                    fprintf(dateizeiger, "%d p_e%02d(%02d_%02d) + %d s_e%02d_plus(%02d_%02d) - %d s_e%02d_minus(%02d_%02d) -f_%02d_%02d(e%02d) >= 0\n", KAPAZITÄT, kant, knote_i, knote_j, KAPAZITÄT, kant, knote_i, knote_j, KAPAZITÄT, kant, knote_i, knote_j, knote_i, knote_j, kant);
+        fprintf(dateizeiger, "\n");
+    }
+
 
     // Speichern Sie die Textdatei
     fprintf(dateizeiger,"End\n");
